4efe49f6a69381e981d7d90e60073d48a0e9fab6
Ashish Kumar <ashish.kumar@vdx.tv>
2025-03-02 01:38:22 +0530

    Ai based banner image generation added

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -7,6 +7,7 @@ export const TEXT_MODEL = "mistralai/Mistral-7B-Instruct-v0.3"; // Text model
 export const IMAGE_MODELS = [
   "nDimensional/NatVis-Natural-Vision-SDXL",
   "martintomov/naturalkata-flux",
-  "CompVis/stable-diffusion-v1-4"
+  "CompVis/stable-diffusion-v1-4",
+  "stabilityai/stable-diffusion-xl-base-1.0"
 ];
-export const IMAGE_MODEL = IMAGE_MODELS[1]; // Default to second model
+export const IMAGE_MODEL = IMAGE_MODELS[3]; // Default to second model
