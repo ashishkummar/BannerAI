1d1373c5eb94af77467c5e7908a69937fcf15248
Ashish Kumar <ashish.kumar@vdx.tv>
2025-03-03 02:09:10 +0530

    Typing effect added for default loaded prompt

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -6,6 +6,14 @@ export const HF_API_KEY = process.env.HUGGINGFACE_API_KEY;
 export const TEXT_MODEL = "mistralai/Mistral-7B-Instruct-v0.3"; // Text model
 
 export const IMAGE_MODELS = [
+  "stabilityai/stable-diffusion-2-1-unclip",
+  "stabilityai/stable-diffusion-3.5-large-turbo",
+  "stabilityai/stable-diffusion-3.5-medium",
+  "stabilityai/stable-diffusion-2-1-base",
+  "stabilityai/stable-diffusion-xl-base-0.9",
+  "stabilityai/stable-diffusion-2",
+  "stabilityai/stable-diffusion-3-medium",
+  "stabilityai/stable-diffusion-3.5-large",
   "stabilityai/stable-diffusion-xl-base-1.0",
   "stabilityai/stable-diffusion-2-1",
   "nDimensional/NatVis-Natural-Vision-SDXL",
